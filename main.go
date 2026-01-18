f8d0169d787b2bedf2e83d08b23d3742e0959704
Surya Suharna <19623125@std.stei.itb.ac.id>
2026-01-18 23:44:09 +0700

    Add home controller and homepage template

--- main.go
+++ main.go
@@ -4,10 +4,14 @@ import (
 	"log"
 	"net/http"
 	"web-go-native/config"
+	"web-go-native/controller/homecontroller"
 )
 
 func main() {
 	config.ConnectDB()
+
+	// 1. Homepage
+	http.HandleFunc("/", homecontroller.Welcome)
 
 	log.Println("Server running on port 8080")
 	http.ListenAndServe(":8080", nil)
