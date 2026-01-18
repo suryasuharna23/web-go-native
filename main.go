53beebc611520d5b949d624448fbf541fa4fbb86
Surya Suharna <19623125@std.stei.itb.ac.id>
2026-01-19 01:14:20 +0700

    feat: implement category controller and routes

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 	"web-go-native/config"
+	"web-go-native/controller/categorycontroller"
 	"web-go-native/controller/homecontroller"
 )
 
@@ -12,6 +13,12 @@ func main() {
 
 	// 1. Homepage
 	http.HandleFunc("/", homecontroller.Welcome)
+
+	// 2. Category
+	http.HandleFunc("/catgories", categorycontroller.Index)
+	http.HandleFunc("/category/add", categorycontroller.Add)
+	http.HandleFunc("/category/edit", categorycontroller.Edit)
+	http.HandleFunc("/category/delete", categorycontroller.Delete)
 
 	log.Println("Server running on port 8080")
 	http.ListenAndServe(":8080", nil)
